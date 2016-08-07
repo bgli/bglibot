e6dab15d952d1c851b62ede909697773551e2c61
Sucipto <show.cheap@gmail.com>
2016-08-08 06:41:10 +0700

    Tambah plugin rules

--- php/longpolling.php
+++ php/longpolling.php
@@ -6,9 +6,9 @@ while (true)
 {
 	sleep(2);
  	$update_id  = 0; //mula-mula tepatkan nilai offset pada nol
-         
+
 	//cek file apakah terdapat file "last_update_id"
-	if (file_exists("last_update_id")) 
+	if (file_exists("last_update_id"))
 	{
 	       //jika ada, maka baca offset tersebut dari file "last_update_id"
 	       $update_id = (int)file_get_contents("last_update_id");
@@ -20,13 +20,13 @@ while (true)
     {
 		$update_id = $message['update_id'];
 		$message_data = $message['message'];
-		
+
 		$bot->chat_id = isset($message_data['chat']['id'])?$message_data['chat']['id']:'';
 		$bot->reply_id = isset($message_data['message_id'])?$message_data['message_id']:'';
-	   
-	   // Membaca seluruh plugin dan function nya		    		
+
+	   // Membaca seluruh plugin dan function nya
 			for ($i=0; $i < count($plugin_name) ; $i++)
-	        { 
+	        {
 				$plugin_function = 'call_' . $plugin_name[$i];
 				if (function_exists($plugin_function))
 				{
@@ -34,13 +34,12 @@ while (true)
 					if ($pResult->error == false)
 					{
 						//untuk debuging, pengecekan apakah bot sudah benar-benar terkirim
-						print_r($pResult->result);	
+						print_r($pResult->result);
 					}
-				}	
-			}			
-	                    
-	}
-            
-	file_put_contents("last_update_id", $update_id + 1);
-} 
+				}
+			}
 
+	}
+
+	file_put_contents("last_update_id", $update_id + 1);
+}
